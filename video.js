48a367d4065c4588196ae3f00c7fbb6ca76f4b26
julien chameroy <julienchameroy@MacBook-Pro-de-julien.local>
2020-11-03 16:18:26 +0100

    creation de tous les tags en fonction de la durée de la video

--- video.js
+++ video.js
@@ -1,33 +1,24 @@
 var context, video, width, height, canvas;
 var allTags = new Array(); 
 var tagexist;
-var existAtime;
 var press = true;
-// travailler avec des blobs
-var video_data = new Array();
+const video_data = { "test": "donnees test" };
 const file = new Blob(
   [JSON.stringify(video_data)], 
   { type: 'application/json' }
 );
-// url qui sera a telecharger
 const fileURL = URL.createObjectURL(file);
 var linkElement;
 
-function Tag(w, h, color, x, y) {
+function Tag(tagname, w, h, color, x, y, currentTime) {
   this.w = w;
   this.h = h;
   this.x = x;
   this.y = y; 
-  this.currentTime = canvas.previousElementSibling.currentTime;
-  this.tagname = prompt("nom du tag :");
+  this.currentTime = currentTime;
+  this.tagname = "" ? prompt("nom du tag :") : tagname;
   context.fillStyle = color;
   context.fillRect(this.x, this.y, this.w, this.h);
-  video_data.push({ 
-    "tagname": this.tagname, 
-    "currentime": this.currentTime,
-    "x": x,
-    "y": y
-  })
 }
 
 function timerCallback () {
@@ -40,8 +31,6 @@ function timerCallback () {
       self.timerCallback();
     }, 0);
 }
-
-// TODO - creation/upload "propres" de fichiers
 
 function videoToFrame() {
   video = document.getElementById("video");
@@ -62,29 +51,25 @@ function videoToFrame() {
 
 function getFrames() {
   context.drawImage(this.video, 0, 0, this.width, this.height);
-  if (video_data.length>0) {
-    video_data = uniqBy(video_data, JSON.stringify)
-    // regarder filter nom et plus récent
-    existAtime = video_data.filter(tag => video.currentTime.toFixed(0) === tag.currentime.toFixed(0));
-    // existAtime = video_data.filter(tag => video.currentTime >= tag.currentime);
+  if (allTags.length>0) {
+    var existAtime = allTags.filter(tag => video.currentTime.toFixed(0) === tag.currentTime.toFixed(0));
     if (existAtime.length>0) {
       existAtime.map(function(tag){
         context.fillStyle = "red";
-        context.fillRect(tag.x, tag.y, 30, 30);
+        context.fillRect(tag.x, tag.y, tag.w, tag.h);
       });  
     }
   }
 }
 
-// TODO - A découper
 function createIfNotExist(mx, my) {
-  tagexist = video_data.filter(tag => ((mx>=tag.x&&mx<=tag.x+30)&&(my>=tag.y&&my<=tag.y+30)));
+  tagexist = allTags.filter(tag => ((mx>=tag.x&&mx<=tag.x+30)&&(my>=tag.y&&my<=tag.y+30)));
   if (tagexist.length>0) {
     canvas.addEventListener('mousemove', function(e) {
       if (press === true) {
         tagexist[0].x = e.layerX;
         tagexist[0].y = e.layerY;
-        tagexist[0].currentime = video.currentTime;
+        tagexist[0].currentTime = video.currentTime;
       }
     })
     canvas.addEventListener('mousedown', function(e) {
@@ -94,33 +79,16 @@ function createIfNotExist(mx, my) {
     canvas.addEventListener('mouseup', function(e) {
       press = false;
       console.log('mouseup');
-      // test length > 0
-      var sameName = allTags.filter(tag => tagexist[0].tagname === tag.tagname)
-      if (sameName.length>0) { 
-        video_data.push({ 
-          "tagname": sameName[0].tagname, 
-          "currentime": sameName[0].currentTime,
-          "x": sameName[0].x,
-          "y": sameName[0].y
-        })
-        sameName[0].tagname = tagexist[0].tagname;
-        sameName[0].currentTime = tagexist[0].currentime;
-        sameName[0].x = tagexist[0].x;
-        sameName[0].y = tagexist[0].y;
-      }
-      video_data.push({ 
-        "tagname": tagexist[0].tagname, 
-      "tagname": tagexist[0].tagname, 
-        "tagname": tagexist[0].tagname, 
-        "currentime": tagexist[0].currentime,
-        "x": tagexist[0].x,
-        "y": tagexist[0].y
-      })
     })
   console.log(tagexist[0].tagname);
   }
   if (tagexist.length<=0) {
-    allTags.push(new Tag(30, 30, "red", mx, my));      
+    var name = prompt("nom du tag :")
+    if (name !== "") {
+      for (var i = parseInt(video.currentTime.toFixed(0)); i < parseInt(video.duration.toFixed(0)); i++) {
+        allTags.push(new Tag(name, 30, 30, "red", mx, my, i));              
+      }
+    }
   }
   return tagexist;
 }
@@ -133,14 +101,6 @@ function deleteTag(mx, my) {
   }
 }
 
-function uniqBy(a, key) {
-    var seen = {};
-    return a.filter(function(item) {
-        var k = key(item);
-        return seen.hasOwnProperty(k) ? false : (seen[k] = true);
-    })
-}
-
 videoToFrame()
 
 if (canvas !== null) {
@@ -149,13 +109,17 @@ if (canvas !== null) {
     if (allTags.length>0) {
       createIfNotExist(e.layerX, e.layerY);
     } else {
-      allTags.push(new Tag(30, 30, "red", e.layerX, e.layerY));      
+      var name = prompt("nom du tag :")
+      if (name !== "") {
+        for (var i = parseInt(video.currentTime.toFixed(0)); i < parseInt(video.duration.toFixed(0)); i++) {
+          allTags.push(new Tag(name, 30, 30, "red", e.layerX, e.layerY, i));              
+        }
+      }
     }
   })
   canvas.addEventListener('contextmenu', function(e) {
-    // TODO - alert avec possibilité d'annuler 
     e.preventDefault();
-    if (video_data.length>0) {
+    if (allTags.length>0) {
       deleteTag(e.layerX, e.layerY);
     }
   })
